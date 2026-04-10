37c57371c3a3e3f971e258caf37be63fbe0d400b
Yair Barraza <barrazayair12345@gmail.com>
2026-04-09 18:25:36 -0600

    awgnChannel 1.1

--- SequentialDecoding/awgnChannel.py
+++ SequentialDecoding/awgnChannel.py
@@ -1,13 +1,11 @@
 import numpy as np
 
-
 def awgn_channel(bits, ebn0_db, rate):
     '''
     Simulate AWGN channel and return llrs
     '''
 
     # BPSK Mapping
-    # 1 is assigned -1, 0 is assigned 1
     x = 1 - 2 * bits
 
     # Eb/N0(dB) to sigma2 conversion
@@ -17,13 +15,14 @@ def awgn_channel(bits, ebn0_db, rate):
     sigma = np.sqrt(sigma2)
 
     # AWGN Noise
-    noise = np.random.normal(0.0, sigma, size = x.shape)
-    y = x  + noise
+    noise = np.random.normal(0.0, sigma, size=x.shape)
+    y = x + noise
 
     # LLRs
-    llr = (2.0 / sigma2) * y
+    llr = 4 * esn0_lin * y
 
     return llr
+
 
 def vector_similarity_test(v1, v2, tol):
     for i in range(len(v1)):
@@ -31,19 +30,19 @@ def vector_similarity_test(v1, v2, tol):
             return False
     return True
 
-''' Testing to ensure different outputs'''
+"""
+''' Testing to ensure different outputs '''
 num_trials = 1000
-bits = np.zeros(128, dtype= int)
+bits = np.zeros(128, dtype=int)
 ebn0_db = 2
 rate = 0.5
-
 vec_list = []
 similar_count = 0
 tolerance = 0.1
 
 for trial in range(num_trials):
     llr = awgn_channel(bits, ebn0_db, rate)
-    
+
     # Adjusting the size of the comparison vectors
     vec = np.round(llr[:5], 1)
 
@@ -56,3 +55,4 @@ for trial in range(num_trials):
 print(f"Total trials: {num_trials}")
 print(f"Number of similar vector LLR patterns: {similar_count}")
 print(f"Number of unique patterns: {num_trials - similar_count}")
+"""
